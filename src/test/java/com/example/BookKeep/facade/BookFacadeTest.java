bf0b5d5ea0078afe266deb2bc27083cd51e8e689
mitka9091 <mitka9091@gmail.com>
2023-11-08 14:31:55 +0100

    Created two test methods: testGetAllBooksWhenDataExists, testGetAllBooksWhenNoDataExists

--- src/test/java/com/example/BookKeep/facade/BookFacadeTest.java
+++ src/test/java/com/example/BookKeep/facade/BookFacadeTest.java
@@ -2,6 +2,7 @@ package com.example.BookKeep.facade;
 
 import com.example.BookKeep.dto.AuthorDTO;
 import com.example.BookKeep.dto.BookDTO;
+import com.example.BookKeep.model.Author;
 import com.example.BookKeep.model.Book;
 import com.example.BookKeep.service.BookServiceImpl;
 import org.junit.Test;
@@ -9,20 +10,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -88,15 +90,35 @@ public class BookFacadeTest {
         Book book1 = new Book();
         book1.setId(1L);
         book1.setTitle("Book 1");
+        Author author1 = new Author();
+        author1.setId(1L);
+        author1.setName("Author 1");
+        book1.setAuthor(author1);
 
         Book book2 = new Book();
         book2.setId(2L);
         book2.setTitle("Book 2");
+        Author author2 = new Author();
+        author2.setId(2L);
+        author2.setName("Author 2");
+        book2.setAuthor(author2);
 
-        when(bookServiceImpl.getAllBooks()).thenReturn(Arrays.asList(book1, book2));
+        Mockito.when(bookServiceImpl.getAllBooks()).thenReturn(Arrays.asList(book1,book2));
 
-        List<BookDTO> bookDTOs = bookFacade.getAllBooks();
+        List<BookDTO> bookDTOs = bookFacadeImpl.getAllBooks();
 
+        assertEquals(2, bookDTOs.size());
+        assertEquals("Book 1", bookDTOs.get(0).getTitle());
+        assertEquals("Book 2", bookDTOs.get(1).getTitle());
+    }
+
+    @Test
+    public void testGetAllBooksWhenNoDataExists(){
+        Mockito.when(bookServiceImpl.getAllBooks()).thenReturn(Collections.emptyList());
+
+        List<BookDTO> bookDTOs = bookFacadeImpl.getAllBooks();
+
+        assertTrue(bookDTOs.isEmpty());
     }
 }
 
